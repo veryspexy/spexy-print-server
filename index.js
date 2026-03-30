5874a70db2d40a86ae48b5a8ea688e99b7f44071
Jon Mitchell <boutique@veryspexy.co.uk>
2026-03-30 17:34:08 +0100

    fix: use PrintRequestInfo Version 3.00 for TM-T88VI

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/print', async (req, res) => {
       });
 
       const safeContent = job.content;
-      const xml = '<?xml version="1.0" encoding="utf-8"?><PrintRequestInfo Version="2.00"><ePOSPrint><Parameter><devid>local_printer</devid><timeout>30</timeout></Parameter><PrintData>' + safeContent + '</PrintData></ePOSPrint></PrintRequestInfo>';
+      const xml = '<?xml version="1.0" encoding="utf-8"?><PrintRequestInfo Version="3.00"><ePOSPrint><Parameter><devid>local_printer</devid><timeout>30</timeout></Parameter><PrintData>' + safeContent + '</PrintData></ePOSPrint></PrintRequestInfo>';
 
       console.log('Sending job', job.id, 'to', location);
       console.log('XML:', xml);
